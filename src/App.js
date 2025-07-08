9435cf2675d3179a39b6df8540674255881d665d
mkrzysztoforski <kontakt.mk@hotmail.com>
2025-07-08 10:22:24 +0200

    Add background image and integrate Kore Web SDK in App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+  const script1 = document.createElement('script');
+  script1.src = 'https://cdn.jsdelivr.net/npm/kore-web-sdk@11.14.1/dist/umd/kore-web-sdk-umd-chat.min.js';
+  script1.async = true;
+
+  script1.onload = () => {
+    if (window.KoreChatSDK) {
+      window.KoreChatSDK.chatConfig.botOptions.API_KEY_CONFIG.KEY = 'd899143953bf4a23a082b0de3502f7273a934aedc2804987a16d2ce0fdc6e5b0stfd';
+      new window.KoreChatSDK.chatWindow().show(window.KoreChatSDK.chatConfig);
+    } else {
+      console.error('KoreChatSDK not found on window');
+    }
+  };
+
+  document.body.appendChild(script1);
+
+  return () => {
+    document.body.removeChild(script1);
+  };
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      style={{
+        backgroundImage: `url('/bg.png')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        width: '100vw',
+        height: '100vh',
+      }}
+    />
   );
 }
 
